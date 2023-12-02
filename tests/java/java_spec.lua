03062dd5385b703913860ce50ae33f7fca8188ed
s1n7ax <srineshnisala@gmail.com>
2023-12-03 01:17:38 +0530

    fix(test): fix tests after dap API transfer

--- tests/java/java_spec.lua
+++ tests/java/java_spec.lua
@@ -1,5 +1,5 @@
 describe('setup', function()
 	it('get_config API is available', function()
-		assert(require('java-core.dap'), 'dap module is available')
+		assert(require('java-core.ls.servers.jdtls'), 'dap module is available')
 	end)
 end)
