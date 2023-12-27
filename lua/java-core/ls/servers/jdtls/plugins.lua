8f9b9acf485c7705dbffb9b6ca76e93196d00b10
s1n7ax <srineshnisala@gmail.com>
2023-12-27 21:08:45 +0530

    fix: trying to load jar that's not a jdtls extension

--- lua/java-core/ls/servers/jdtls/plugins.lua
+++ lua/java-core/ls/servers/jdtls/plugins.lua
@@ -1,32 +1,43 @@
 local mason = require('java-core.utils.mason')
-local path = require('java-core.utils.path')
 local file = require('java-core.utils.file')
 
 local List = require('java-core.utils.list')
 
 local M = {}
 
-local plugin_to_jar_path_map = {
-	['java-test'] = '*.jar',
-	['java-debug-adapter'] = '*.jar',
+local plug_jar_map = {
+	['java-test'] = {
+		'junit-jupiter-api_*.jar',
+		'junit-jupiter-engine_*.jar',
+		'junit-jupiter-migrationsupport_*.jar',
+		'junit-jupiter-params_*.jar',
+		'junit-platform-commons_*.jar',
+		'junit-platform-engine_*.jar',
+		'junit-platform-launcher_*.jar',
+		'junit-platform-runner_*.jar',
+		'junit-platform-suite-api_*.jar',
+		'junit-platform-suite-commons_*.jar',
+		'junit-platform-suite-engine_*.jar',
+		'junit-vintage-engine_*.jar',
+		'org.apiguardian.api_*.jar',
+		'org.eclipse.jdt.junit4.runtime_*.jar',
+		'org.eclipse.jdt.junit5.runtime_*.jar',
+		'org.opentest4j_*.jar',
+		'com.microsoft.java.test.plugin-*.jar',
+	},
+	['java-debug-adapter'] = { '*.jar' },
 }
 
 ---Returns a list of .jar file paths for given list of jdtls plugins
----@param plugins string[]
+---@param plugin_names string[]
 ---@return string[] # list of .jar file paths
-function M.get_plugin_paths(plugins)
-	local plugin_paths = List:new()
-
-	for _, plugin in ipairs(plugins) do
-		local relative_path = plugin_to_jar_path_map[plugin]
-		local plugin_shared_path = mason.get_shared_path(plugin)
-		local full_path = path.join(plugin_shared_path, relative_path)
-		local resolved_paths = file.get_file_list(full_path)
-
-		plugin_paths:push(resolved_paths)
-	end
-
-	return plugin_paths:flatten()
+function M.get_plugin_paths(plugin_names)
+	return List:new(plugin_names)
+		:map(function(plugin_name)
+			local root = mason.get_shared_path(plugin_name)
+			return file.resolve_paths(root, plug_jar_map[plugin_name])
+		end)
+		:flatten()
 end
 
 return M
