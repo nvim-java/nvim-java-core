295161308d57cc4c7a69daeeee6951e74080c661
Srinesh Nisala <srineshnisala@gmail.com>
2023-12-27 21:16:22 +0530

    fix: trying to load jar that's not a jdtls extension (#56)

--- lua/java-core/utils/file.lua
+++ lua/java-core/utils/file.lua
@@ -1,47 +1,15 @@
+local path = require('java-core.utils.path')
+
+local List = require('java-core.utils.list')
+
 local M = {}
 
----Return true if the given path is a directory
----@param path string
----@return boolean
-function M.is_dir(path)
-	return vim.fn.isdirectory(path) == 1
-end
-
----Returns true if the given path is a file
----@param path string
----@return boolean
-function M.is_file(path)
-	return vim.fn.filereadable(path) == 1
-end
-
----Returns true if the given path is an executable
----@param path string
----@return boolean
-function M.is_exec(path)
-	return vim.fn.executable(path) == 1
-end
-
----Returns a list of files from a path with wildcards
----@param path string path with the wildcards
----@return string[]
-function M.get_file_list(path)
-	return vim.fn.glob(path, true, true)
-end
-
----Creates a directory in the given path
----@param path string
----@param opts JavaDirCreateFlags
-function M.mkdir(path, opts)
-	local flags = ''
-
-	if opts.create_intermediate then
-		flags = flags .. 'p'
-	end
-
-	vim.fn.mkdir(path, flags)
+function M.resolve_paths(root, paths)
+	return List:new(paths)
+		:map(function(path_pattern)
+			return vim.fn.glob(path.join(root, path_pattern), true, true)
+		end)
+		:flatten()
 end
 
 return M
-
----@class JavaDirCreateFlags
----@field create_intermediate boolean whether the intermediate directories should be created in the process
